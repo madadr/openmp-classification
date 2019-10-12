253bb91d6756247c824a340088037bead02b1164
Adrian Madej <adrian.madej@ericsson.com>
2019-10-12 10:53:42 +0200

    Working KNN on letter recognition dataset

--- main.cpp
+++ main.cpp
@@ -11,24 +11,42 @@ namespace
 {
 using namespace std;
 
-static constexpr uint32_t ATTRIBUTES = 11;
+static constexpr uint32_t ATTRIBUTES = 16;
 static constexpr uint32_t MATRIX_SIZE = ATTRIBUTES + 1; // attributes + its class
 
-// static constexpr uint32_t TRAIN_SET_SIZE = 5;
-static constexpr uint32_t TRAIN_SET_SIZE = 1000;
-// static constexpr uint32_t TEST_SET_SIZE = 5;
-static constexpr uint32_t TEST_SET_SIZE = 499;
+static constexpr uint32_t SET_SIZE = 20000;
+
+static constexpr uint32_t TRAIN_SET_SIZE = SET_SIZE * 0.9;
+static constexpr uint32_t TEST_SET_SIZE = SET_SIZE * 0.1;
+
+vector<char> letters;
 }
 
 vector<vector<double>> fetchDatasetFromFile()
 {
-    // Wine quality dataset
-    // https://archive.ics.uci.edu/ml/datasets/Wine+Quality
+    // Letter recognition dataset
+    // https://archive.ics.uci.edu/ml/datasets/Letter+Recognition
     // CSV format:
-    // "fixed acidity";"volatile acidity";"citric acid";"residual sugar";"chlorides";"free sulfur dioxide";"total sulfur dioxide";"density";"pH";"sulphates";"alcohol";"quality"
-    vector<vector<double>> values{MATRIX_SIZE};
+    // 1. lettr capital letter (26 values from A to Z)
+    // 2. x-box horizontal position of box (integer)
+    // 3. y-box vertical position of box (integer)
+    // 4. width width of box (integer)
+    // 5. high height of box (integer)
+    // 6. onpix total # on pixels (integer)
+    // 7. x-bar mean x of on pixels in box (integer)
+    // 8. y-bar mean y of on pixels in box (integer)
+    // 9. x2bar mean x variance (integer)
+    // 10. y2bar mean y variance (integer)
+    // 11. xybar mean x y correlation (integer)
+    // 12. x2ybr mean of x * x * y (integer)
+    // 13. xy2br mean of x * y * y (integer)
+    // 14. x-ege mean edge count left to right (integer)
+    // 15. xegvy correlation of x-ege with y (integer)
+    // 16. y-ege mean edge count bottom to top (integer)
+    // 17. yegvx correlation of y-ege with x (integer)
+    vector<vector<double>> values(ATTRIBUTES);
 
-    ifstream file("csv/winequality-red.csv");
+    ifstream file("csv/letter-recognition.data");
     string line;
 
     while (getline(file, line))
@@ -36,9 +54,14 @@ vector<vector<double>> fetchDatasetFromFile()
         stringstream stream(line);
         string stringValue; // represents double value
         int position = 0;
-        while (getline(stream, stringValue, ';'))
+        while (getline(stream, stringValue, ','))
         {
-            values.at(position).push_back(stod(stringValue));
+            if (position == 0)
+            {
+                letters.push_back(stringValue.at(0));
+            } else {
+                values.at(position - 1).push_back(stod(stringValue));
+            }
             position = (position + 1) % MATRIX_SIZE;
         }
     }
@@ -46,12 +69,12 @@ vector<vector<double>> fetchDatasetFromFile()
     return values;
 }
 
-std::pair<double, double> findMinMax(vector<double> &dataset)
+std::pair<double, double> findMinMax(vector<double> &attributeSet)
 {
-    double min = dataset.at(0);
+    double min = attributeSet.at(0);
     double max = min;
 
-    for (const auto& value : dataset)
+    for (const auto& value : attributeSet)
     {
         if (value < min)
         {
@@ -67,57 +90,60 @@ std::pair<double, double> findMinMax(vector<double> &dataset)
     return std::make_pair(min, max);
 }
 
-void normalize(vector<double> &dataset)
+void normalize(vector<double> &attributeSet)
 {
-    const auto [min, max] = findMinMax(dataset);
+    const auto [min, max] = findMinMax(attributeSet);
 
     double diff = max - min;
 
-    for (auto& value : dataset)
+    for (auto& value : attributeSet)
     {
         value = (value - min) / diff;
     }
 }
 
-std::pair<double, double> findAverageAndVariation(vector<double> &dataset)
+std::pair<double, double> findAverageAndVariation(vector<double> &attributeSet)
 {
     double average{};
     
-    for (const auto& value : dataset)
+    for (const auto& value : attributeSet)
     {
         average += value;
     }
-    average /= dataset.size();
+    average /= attributeSet.size();
 
     double variation{};
-    for (const auto& value : dataset)
+    for (const auto& value : attributeSet)
     {
         auto tmp = value - average;
         variation += tmp * tmp;
     }
-    variation /= dataset.size(); // variance
+    variation /= attributeSet.size(); // variance
     variation = sqrt(variation);
 
     return std::make_pair(average, variation);
 }
 
-void standarize(vector<double> &dataset)
+void standarize(vector<double> &attributeSet)
 {
-    const auto [average, variation] = findAverageAndVariation(dataset);
+    const auto [average, variation] = findAverageAndVariation(attributeSet);
 
-    for (auto& value : dataset)
+    for (auto& value : attributeSet)
     {
         value = (value - average) / variation;
     }
 }
 
-void knn(vector<vector<double>> dataset)
+void knn(vector<vector<double>>& inputDataset)
 {
     int correct{};
     int minimalDistance{};
     int minimalDistanceIndex{};
     for (int i = TRAIN_SET_SIZE; i < TRAIN_SET_SIZE + TEST_SET_SIZE; ++i)
     {
+        // Copy dataset for each test row
+        auto dataset = inputDataset;
+
         // Calculate squares for every attribute
         for (int j = 0; j < ATTRIBUTES; ++j)
         {
@@ -130,7 +156,7 @@ void knn(vector<vector<double>> dataset)
         }
 
         double minimalSum;
-        double genre;
+        char genre;
         // Sum each row & calculate square root
         for (int k = 0; k < TRAIN_SET_SIZE; ++k)
         {
@@ -142,20 +168,15 @@ void knn(vector<vector<double>> dataset)
 
             sum = sqrt(sum);
 
-            // std::cout << "sum = " << sum << std::endl;
-            // std::cout << "minimalSum = " << minimalSum << std::endl;
-            // std::cout << "genre bfr = " << genre << std::endl;
             if (k == 0 || sum < minimalSum)
             {
                 minimalSum = sum;
-                // std::cout << "minimalSum aft = " << minimalSum << std::endl;
-                genre = dataset.at(MATRIX_SIZE - 1).at(k);
-                // std::cout << "genre aft = " << genre << std::endl;
+                genre = letters.at(k);
             }
         }
 
-        // std::cout << "Row i = " << i << " detected as \n\t" << genre << "\nactual\n\t" << dataset.at(MATRIX_SIZE - 1).at(i) << std::endl;
-        if (genre == dataset.at(MATRIX_SIZE - 1).at(i))
+        // std::cout << "Row i = " << i << " detected as \n\t" << genre << "\nactual\n\t" << letters.at(i) << std::endl;
+        if (genre == letters.at(i))
         {
             ++correct;
         }
@@ -167,12 +188,13 @@ void knn(vector<vector<double>> dataset)
 
 int main()
 {
+    letters.reserve(SET_SIZE);
     auto dataset = fetchDatasetFromFile();
 
     // #pragma omp parallel for
     for (int i = 0; i < ATTRIBUTES; ++i)
     {
-        normalize(dataset.at(i));
+        // normalize(dataset.at(i));
         // standarize(dataset.at(i));
     }
 
